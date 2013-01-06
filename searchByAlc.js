99d88b669e4bdd2a3ac824bca92347f5512beec5
yukofeb <yuko.february@gmail.com>
2013-01-06 20:23:53 +0900

    modify spell

--- searchByAlc.js
+++ searchByAlc.js
@@ -1,4 +1,4 @@
 javascript:
   var d = document;
-  var txt = d.selection? d.selection.createRange().txt: d.getSeletion();
-  var subw = window.open('http://eow.alc.co.jp/'+txt+'/UTF-8/?ref=sa').document;
+  var text = d.selection? d.selection.createRange().txt: d.getSelection();
+  var subw = window.open('http://eow.alc.co.jp/'+text+'/UTF-8/?ref=sa').document;

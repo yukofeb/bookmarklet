17836b66302e640cea0433a21f7e2ac102c987ed
yukofeb <yuko.february@gmail.com>
2013-01-06 20:48:44 +0900

    remove .document

--- searchByAlc.js
+++ searchByAlc.js
@@ -1,4 +1,4 @@
 javascript:
   var d = document;
   var text = d.selection? d.selection.createRange().txt: d.getSelection();
-  var subw = window.open('http://eow.alc.co.jp/'+text+'/UTF-8/?ref=sa').document;
+  var subw = window.open('http://eow.alc.co.jp/'+text+'/UTF-8/?ref=sa');
